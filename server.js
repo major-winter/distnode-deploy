7d8dcdaa18524dae78b8e903897101aecf2ef3f8
major-winter <jason@debian>
2021-11-03 05:48:59 +0700

    Applicaton files

---
+++ server.js
@@ -0,0 +1,16 @@
+const server = require('fastify')()
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 8000
+const Recipe = require('./recipe.js')
+server.get('/', async (req, res) => {
+  return "Hello from Distributed Nodejs!"
+})
+server.get('/recipes/:id', async (req, reply) => {
+  const recipe = new Recipe(req.params.id)
+  await recipe.hydrate()
+  return recipe
+}) 
+
+server.listen(PORT, HOST, (err, host) => {
+  console.log(`Server running at ${host}`)
+})
